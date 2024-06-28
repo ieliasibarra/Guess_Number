22716f3fd2a00804bac28e3d892c11f6569e5d30
Elias_Ibarra <ieliasibarra@gmail.com>
2024-06-27 22:12:03 -0400

    Update funcionalidad boton

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del Numero Secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10 ';
+
+function intentoDeUsuario(){
+    alert(`click desde el boton`);
+}
