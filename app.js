28592195c1f1769a2422e888694e50491982c01b
Elias_Ibarra <ieliasibarra@gmail.com>
2024-06-29 21:01:51 -0400

    update reiniciar juego

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
-
-console.log(numeroSecreto);
+let numeroSecreto = 0;
+let intentos = 0;
 
 function asignarTextoElementos(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -10,32 +8,43 @@ function asignarTextoElementos(elemento, texto) {
 }
 
 function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value) ;
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
-    if(numeroDeUsuario == numeroSecreto){
-        asignarTextoElementos('p', `Acertaste el numero en  ${intentos} ${(intentos == 1)?'vez': 'veces'}`);
+    intentos++; 
+
+    if (numeroDeUsuario == numeroSecreto) {
+        asignarTextoElementos('p', `Acertaste el número en ${intentos} ${(intentos == 1) ? 'vez' : 'veces'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
-    }else{
-        if(numeroDeUsuario > numeroSecreto){
-            asignarTextoElementos('p', 'El numero es menor');
-        }else{
-            asignarTextoElementos('p', 'El numero es mayor');
+    } else {
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElementos('p', 'El número es menor');
+        } else {
+            asignarTextoElementos('p', 'El número es mayor');
         }
-        intentos++;
         limpiarCaja();
     }
     return;
 }
 
-function limpiarCaja(){
-    document.querySelector('#valorUsuario').value = '';
+function condicionesIniciales() {
+    asignarTextoElementos('h1', 'Juego del Número Secreto');
+    asignarTextoElementos('p', 'Indica un Número del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1; 
+}
 
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
 }
 
 function generarNumeroSecreto() {
-    return  Math.floor(Math.random() * 10) + 1;
+    return Math.floor(Math.random() * 10) + 1;
 }
 
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
 
-asignarTextoElementos('h1', 'Juego del Número Secreto');
-asignarTextoElementos('p', 'Indica un Número del 1 al 10');
+condicionesIniciales();
